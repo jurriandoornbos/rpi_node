be606cc5f350436bdc7a3188d5f56e48a3c1f0ec
jurriandoornbos <59558756+jurriandoornbos@users.noreply.github.com>
2022-04-26 12:08:57 +0200

    massiv eupdate

--- src/imu_rgb_stereo_publisher.cpp
+++ src/imu_rgb_stereo_publisher.cpp
@@ -15,8 +15,8 @@
 
 #include <depthai_bridge/ImuConverter.hpp>
 
-static constexpr int fps = 30;
-static constexpr int hz = 200;
+static constexpr int fps = 20;
+
 static constexpr auto monoRes = dai::MonoCameraProperties::SensorResolution::THE_400_P;
 static std::atomic<bool> downscaleColor{true};
 
@@ -26,24 +26,16 @@ dai::Pipeline createPipeline(bool withDepth, bool lrcheck, bool extended, bool s
     std::vector<std::string> queueNames;
 
     // Define sources and outputs
-    auto camRgb = pipeline.create<dai::node::ColorCamera>();
+
     auto left = pipeline.create<dai::node::MonoCamera>();
     auto right = pipeline.create<dai::node::MonoCamera>();
     auto stereo = pipeline.create<dai::node::StereoDepth>();
 
-    auto rgbOut = pipeline.create<dai::node::XLinkOut>();
     auto depthOut = pipeline.create<dai::node::XLinkOut>();
 
-    rgbOut->setStreamName("rgb");
-    queueNames.push_back("rgb");
     depthOut->setStreamName("depth");
     queueNames.push_back("depth");
 
-    // Properties
-    camRgb->setBoardSocket(dai::CameraBoardSocket::RGB);
-    camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
-    camRgb->setFps(fps);
-    if(downscaleColor) camRgb->setIspScale(1, 3);
 
     left->setResolution(monoRes);
     left->setBoardSocket(dai::CameraBoardSocket::LEFT);
@@ -52,37 +44,18 @@ dai::Pipeline createPipeline(bool withDepth, bool lrcheck, bool extended, bool s
     right->setBoardSocket(dai::CameraBoardSocket::RIGHT);
     right->setFps(fps);
 
-    stereo->initialConfig.setConfidenceThreshold(245);
-    // LR-check is required for depth alignment
+    stereo->initialConfig.setConfidenceThreshold(100);
+    stereo -> initialConfig.setMedianFilter(dai::MedianFilter::KERNEL_7x7);
+        // LR-check is required for depth alignment
     stereo->setLeftRightCheck(lrcheck);
-    stereo->setDepthAlign(dai::CameraBoardSocket::RGB);
     stereo->setExtendedDisparity(extended);
     stereo->setSubpixel(subpixel);
+    
 
-    // Linking
-    camRgb->isp.link(rgbOut->input);
     left->out.link(stereo->left);
     right->out.link(stereo->right);
     stereo->disparity.link(depthOut->input);
 
-
-        // Add IMU To the pipeline
-
-    auto imuSensor = pipeline.create<dai::node::IMU>();
-    auto xoutImu = pipeline.create<dai::node::XLinkOut>();
-    xoutImu->setStreamName("imu");
-    
-    // enable ACCELEROMETER_RAW and GYROSCOPE_RAW at 100 hz rate
-    imuSensor->enableIMUSensor({dai::IMUSensor::LINEAR_ACCELERATION, dai::IMUSensor::GYROSCOPE_CALIBRATED, dai::IMUSensor::ROTATION_VECTOR}, 200);
-    // above this threshold packets will be sent in batch of X, if the host is not blocked and USB bandwidth is available
-    imuSensor->setBatchReportThreshold(1);
-    // maximum number of IMU packets in a batch, if it's reached device will block sending until host can receive it
-    // if lower or equal to batchReportThreshold then the sending is always blocking on device
-    // useful to reduce device's CPU load  and number of lost packets, if CPU load is high on device side due to multiple nodes
-    imuSensor->setMaxBatchReports(10);
-
-    // Link plugins IMU -> XLINK
-    imuSensor->out.link(xoutImu->input);
 
     return pipeline;
 }
@@ -116,11 +89,8 @@ int main(int argc, char** argv){
 
     dai::Device device(pipeline);
 
-    auto depthQueue = device.getOutputQueue("depth", 30, false);
-    auto rgbQueue = device.getOutputQueue("rgb", 30, false);
-
-    auto imuQueue = device.getOutputQueue("imu", hz, false);
-
+    auto depthQueue = device.getOutputQueue("depth", fps, false);
+  
     auto calibrationHandler = device.readCalibration();
 
     // this part would be removed once we have calibration-api
@@ -136,19 +106,7 @@ int main(int argc, char** argv){
      
 
     dai::rosBridge::ImageConverter converter(deviceName + "_right_camera_optical_frame", true);
-    auto rgbCameraInfo = converter.calibrationToCameraInfo(calibrationHandler, dai::CameraBoardSocket::RGB, 1280, 720); 
-    dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> rgbPublish(rgbQueue,
-                                                                                    pnh, 
-                                                                                    std::string("color/image"),
-                                                                                    std::bind(&dai::rosBridge::ImageConverter::toRosMsg, 
-                                                                                    &converter, 
-                                                                                    std::placeholders::_1, 
-                                                                                    std::placeholders::_2) , 
-                                                                                    30,
-                                                                                    rgbCameraInfo,
-                                                                                    "color");
-
-    rgbPublish.addPubisherCallback();
+    auto rgbCameraInfo = converter.calibrationToCameraInfo(calibrationHandler, dai::CameraBoardSocket::LEFT, 680, 400); 
 
     dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> depthPublish(depthQueue,
                                                                                      pnh, 
@@ -157,27 +115,11 @@ int main(int argc, char** argv){
                                                                                      &converter, 
                                                                                      std::placeholders::_1, 
                                                                                      std::placeholders::_2) , 
-                                                                                     30,
+                                                                                     fps,
                                                                                      rgbCameraInfo,
                                                                                      "stereo");
 
     depthPublish.addPubisherCallback();
-    dai::rosBridge::ImuConverter imuConverter(deviceName + "_imu_frame");
-    dai::rosBridge::BridgePublisher<sensor_msgs::Imu, dai::IMUData> imuPublish(imuQueue,
-                                                                                    pnh,
-                                                                                    std::string("imu"),
-                                                                                    std::bind(static_cast<void(dai::rosBridge::ImuConverter::*)
-                                                                                    (std::shared_ptr<dai::IMUData>, sensor_msgs::Imu&)
-                                                                                    >(&dai::rosBridge::ImuConverter::toRosMsg),
-                                                                                    &imuConverter,
-                                                                                    std::placeholders::_1,
-                                                                                    std::placeholders::_2),
-                                                                                    hz,
-                                                                                    rgbCameraInfo,
-                                                                                    "imu");
-
-    imuPublish.addPubisherCallback();  
-
     ros::spin();
     
 
